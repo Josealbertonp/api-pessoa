799fc8c050c12d89f622107eceee33c11166b9cd
Josealbertonp <albertoj0988@gmail.com>
2025-04-01 02:14:41 -0300

    feat(entity): cria entidade Pessoa com atributos

---
+++ src/main/java/com/pessoa/apipessoa/ApiPessoaApplication.java
@@ -0,0 +1,14 @@
+package com.pessoa.apipessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ApiPessoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPessoaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pessoa/apipessoa/entity/Pessoa.java
@@ -0,0 +1,36 @@
+package com.pessoa.apipessoa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pessoa")
+@NoArgsConstructor
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "email", length = 30)
+    private String email;
+
+    public Pessoa(long l, String fulano, String number, LocalDate of, String mail) {
+    }
+}
