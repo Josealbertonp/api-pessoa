e47c3eb323c0458e9e1b6decd2e7fc6f69158a44
Josealbertonp <albertoj0988@gmail.com>
2025-04-02 20:05:54 -0300

    fix: editando script init.sql com dados de seed para inicialização do banco

--- init/init.sql
+++ init/init.sql
@@ -3,6 +3,14 @@ CREATE TABLE IF NOT EXISTS pessoa (
     nome VARCHAR(50),
     cpf VARCHAR(11),
     dataNascimento DATE,
-    email VARCHAR(30),
+    email VARCHAR(50),
     PRIMARY KEY (id)
     );
+
+INSERT INTO pessoa (nome, cpf, dataNascimento, email)
+VALUES
+    ('João Silva', '64571181000', '1985-06-15', 'joao.silva@email.com'),
+    ('Maria Oliveira', '11571738029', '1990-04-22', 'maria.oliveira@email.com'),
+    ('Carlos Souza', '78226929091', '1982-11-30', 'carlos.souza@email.com'),
+    ('Ana Pereira', '39390273080', '1995-02-10', 'ana.pereira@email.com'),
+    ('Pedro Costa', '27075511041', '1988-08-08', 'pedro.costa@email.com');
